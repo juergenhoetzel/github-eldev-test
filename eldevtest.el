725afa528b47d1fe95c81be2a430daf014f47bb2
Juergen Hoetzel <juergen@hoetzel.info>
2021-04-20 18:55:04 +0200

    Initial import

---
+++ eldevtest.el
@@ -0,0 +1,18 @@
+;;; eldevtest.el --- just a eldev test -*- lexical-binding: t; -*-
+
+;; This is free and unencumbered software released into the public domain.
+
+;; Author: Jürgen Hötzel <juergen@hoetzel.info>
+;; URL: https://github.com/juergenhoetzel/github-eldev-test
+;; Keywords: comm, processes
+;; Package: github-eldev-test
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "26.1"))
+;; Package-Type: multi
+
+;; Package: github-eldev-test
+
+;;; Commentary:
+
+(provide 'eldevtest)
+;;; eldevtest.el ends here
